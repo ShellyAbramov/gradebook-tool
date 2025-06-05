101d6c510fc7dc2c9ad604787fe7dcccd5b009ea
Shelly Abramov <shellyabramov18@gmail.com>
2025-06-04 20:34:30 -0400

    Added a function that helps calculate stats such as min, max, and average grade.

--- student_gradebook.py
+++ student_gradebook.py
@@ -13,3 +13,22 @@ def student_info():
 #     print(f"Student {student_full_name} with grade {grade} added successfully.") #for testing purposes
 # student_info() #call the function to get student information #for testing purposes
     
+def student_gradebook_stats():
+    """Function to display the stats of student gradebook including min, max, and average grade."""
+    if not student_grades:
+        print("No students in the gradebook.")
+    else:
+        print("\nStudent Gradebook Statistics:")
+        grades = student_grades.values()
+        length = len(grades)
+        if length > 0:
+            min_grade = min(grades)
+            max_grade = max(grades)
+            avg_grade = sum(grades) / length
+            print(f"Minimum Grade: {min_grade}")
+            print(f"Maximum Grade: {max_grade}")
+            print(f"Average Grade: {avg_grade:.2f}")
+        else:
+            print("No grades available to calculate statistics.")
+
+student_gradebook_stats()  # Call the function to display stats initially #testing purposes
